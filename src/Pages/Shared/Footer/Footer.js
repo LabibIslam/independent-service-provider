1ec4954b23d9f33cce7945c8b568ed24ea975b15
LabibIslam <labib.islam.practice@gmail.com>
2022-04-18 16:07:45 +0600

    added custon data and some images.

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p><small>copyright</small></p>
+            <p><small>copyright @ 2022</small></p>
         </footer>
     );
 };
