34fe434150f8b05b3a69915c19d52241c7c5af01
LabibIslam <labib.islam.practice@gmail.com>
2022-04-19 22:25:30 +0600

    added a way to handle error

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import google from '../../../images/googleicon.png';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorElement;
+    if (error) {
+        errorElement = <div>
+            <p className='text-danger'> Error: {error.message}</p>
+        </div>
+    }
+    if (user) {
+        navigate('/');
+    }
+
     return (
         <div>
             <div className='d-flex align-items-center'>
                 <div style={{ height: '1px' }} className='bg-dark w-50'></div>
                 <p className='mt-2 px-2'>OR</p>
                 <div style={{ height: '1px' }} className='bg-dark w-50'></div>
+            </div>
+            {errorElement}
+            <div >
+                <button
+                    onClick={() => signInWithGoogle()}
+                    className='btn btn-light d-block mx-auto'>
+                    <img style={{ width: '24px' }} src={google} alt="" />
+                    Google Sign In
+                </button>
             </div>
         </div>
     );
